8b7bb53f0356d4f3e214a65455ac4eba1da7fe2a
Chris Sinco <csinco.design@gmail.com>
2023-06-27 22:53:33 -0700

    Hook up to new desktop module

--- desktopApp/src/jvmMain/kotlin/Main.kt
+++ desktopApp/src/jvmMain/kotlin/Main.kt
@@ -1,15 +1,26 @@
 import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.singleWindowApplication
+import data.Templates
+import ui.Playground
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() =
     singleWindowApplication(
-        title = "Falling Balls",
+        title = "Compose Modifiers Playground",
         state = WindowState(size = DpSize(800.dp, 800.dp))
     ) {
-        Text("Hello")
+        val defaultTemplate = Templates.Sun
+        var activeTemplate by remember { mutableStateOf(defaultTemplate) }
+
+        Playground(activeTemplate, onTemplateChange = {
+            activeTemplate = it.copy()
+        })
     }
