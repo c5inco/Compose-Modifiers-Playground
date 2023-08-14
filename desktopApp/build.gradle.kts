9851989e8cc61cb1b5688b6f0823f7ddcffe200c
Chris Sinco <csinco.design@gmail.com>
2023-08-13 21:20:34 -0700

    Fix desktop icon and versioning

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
+fun properties(key: String) = providers.gradleProperty(key)
+
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
@@ -22,9 +24,12 @@ compose.desktop {
         mainClass = "MainKt"
 
         nativeDistributions {
+            macOS {
+                iconFile.set(project.file("src/jvmMain/resources").resolve("META-INF/macosicon.icns"))
+            }
             targetFormats(TargetFormat.Dmg)
-            packageName = "Falling Balls"
-            packageVersion = "1.0.0"
+            packageName = properties("pluginGroup").get()
+            packageVersion = properties("pluginVersion").get()
         }
     }
 }
