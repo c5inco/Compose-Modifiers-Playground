629a385cfabd5a4e50ddac387755396ff0db7210
Chris Sinco <csinco@google.com>
2023-08-21 14:10:44 -0700

    Delete old plugin code in favor of multiplatform organization

--- src/main/kotlin/com/c5inco/modifiers/plugin/PluginIcons.kt
+++
@@ -1,8 +0,0 @@
-package icons
-
-import com.intellij.openapi.util.IconLoader
-
-object PluginIcons {
-    @JvmField
-    val PlaygroundAction = IconLoader.getIcon("/icons/menu-icon.svg", javaClass)
-}
