3c6ffd244976cdaffb784ea5e6906d6327845748
Chris Sinco <csinco.design@gmail.com>
2025-12-14 21:29:25 -0800

    Remove action icon

--- ideaPlugin/src/main/kotlin/PluginIcons.kt
+++
@@ -1,8 +0,0 @@
-package com.c5inco.modifiers.plugin
-
-import com.intellij.openapi.util.IconLoader
-
-object PluginIcons {
-    @JvmField
-    val PlaygroundAction = IconLoader.getIcon("/icons/menu-icon.svg", javaClass)
-}
